207c84d80f0a6ac25eb46cfaf1171cb2b0f9ce00
Ardit Daku <arditdaku1@gmail.com>
2021-07-25 08:14:55 +0200

    Added Font

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -5,9 +5,13 @@ const MenuItem = ({ title, subtitle, imageUrl, size, linkUrl }) => (
     style={{ backgroundImage: `url(${imageUrl})` }}
     className={`${size} menu-item`}
   >
+    <div
+      style={{ backgroundImage: `url(${imageUrl})` }}
+      className="background-image"
+    />
     <div className="content">
       <h1 className="title">{title}</h1>
-      <span className="subtitle">{subtitle}</span>
+      <span className="subtitle">SHOP NOW</span>
     </div>
   </div>
 );
