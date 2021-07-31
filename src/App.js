0a2c6e326791c00c8adf1a7d78261e293ee76ca0
Ardit Daku <arditdaku1@gmail.com>
2021-07-31 20:43:01 +0200

    Adde Images & Collections List & Items

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
 import React from "react";
-import { Switch, Route,Link } from "react-router-dom";
+import {Switch, Route} from "react-router-dom";
 import "./index.css";
 import HomePage from "./pages/homepage/homepage.component";
-const HatsPage = (props) => {
-        console.log(props)
-   return <div className="flex flex-col justify-center items-center ">
-       <Link to='/'>Home</Link>
-        <h1 className="font-bold text-2xl">Hats page</h1>
-       <h3 className="font-semibold text-xl">Hat number {props.match.params.id}</h3>
-    </div>
-};
+import ShopPage from "./pages/shop/shop.component";
 
 function App() {
-  return (
-    <div>
-      <Switch>
-        <Route exact={true} path="/" component={HomePage} />
-        <Route exact={true} path="/shop/hats" component={HatsPage} />
-      </Switch>
-    </div>
-  );
+    return (
+        <div>
+            <Switch>
+                <Route exact={true} path="/" component={HomePage}/>
+                <Route exact={true} path="/shop" component={ShopPage}/>
+            </Switch>
+        </div>
+    );
 }
 
 export default App;
