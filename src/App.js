ab27fad3c1231cf713dc4dc8be06124b0e552623
Ardit Daku <arditdaku1@gmail.com>
2021-07-31 18:11:47 +0200

    Added React Router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from "react";
+import { Switch, Route } from "react-router-dom";
 import "./index.css";
 import HomePage from "./pages/homepage/homepage.component";
+const HatsPage = () => (
+  <div className="flex justify-center items-center ">
+    <h1 className="font-bold text-2xl">Hats page</h1>
+  </div>
+);
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact={true} path="/" component={HomePage} />
+        <Route exact={true} path="/hats" component={HatsPage} />
+      </Switch>
     </div>
   );
 }
