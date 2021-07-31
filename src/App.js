40a6bacfcd66d9b3815e1922493ccb5795173aa9
Ardit Daku <arditdaku1@gmail.com>
2021-07-31 19:26:25 +0200

    Updated our menu-items to navigate withRouter

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import React from "react";
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route,Link } from "react-router-dom";
 import "./index.css";
 import HomePage from "./pages/homepage/homepage.component";
-const HatsPage = () => (
-  <div className="flex justify-center items-center ">
-    <h1 className="font-bold text-2xl">Hats page</h1>
-  </div>
-);
+const HatsPage = (props) => {
+        console.log(props)
+   return <div className="flex flex-col justify-center items-center ">
+       <Link to='/'>Home</Link>
+        <h1 className="font-bold text-2xl">Hats page</h1>
+       <h3 className="font-semibold text-xl">Hat number {props.match.params.id}</h3>
+    </div>
+};
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact={true} path="/" component={HomePage} />
-        <Route exact={true} path="/hats" component={HatsPage} />
+        <Route exact={true} path="/shop/hats" component={HatsPage} />
       </Switch>
     </div>
   );
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,18 +1,19 @@
 import React from "react";
 import "./menu-item.styles.scss";
-const MenuItem = ({ title, subtitle, imageUrl, size, linkUrl }) => (
-  <div className={`${size} menu-item`}>
-    <div
-      style={{ backgroundImage: `url(${imageUrl})` }}
-      className="background-image"
-    />
-    <div className="content">
-      <h1 className="title">{title}</h1>
-      <span className="subtitle">SHOP NOW</span>
+import {withRouter} from 'react-router-dom'
+
+const MenuItem = ({title, subtitle, imageUrl, size, linkUrl, history,match}) => (
+    <div className={`${size} menu-item`} onClick={()=>history.push(`${match.url}${linkUrl}`)}>
+        <div
+            style={{backgroundImage: `url(${imageUrl})`}}
+            className="background-image"
+        />
+        <div className="content">
+            <h1 className="title">{title}</h1>
+            <span className="subtitle">SHOP NOW</span>
+        </div>
     </div>
-  </div>
 );
 
-export default MenuItem;
+export default withRouter(MenuItem);
 
-// mir se ardh e qka do qe bohet koft hajr
