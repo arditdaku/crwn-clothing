abf5a7fb7431a1a4d5e939521c0fcefddb2aeea9
Ardit Daku <arditdaku1@gmail.com>
2021-09-30 11:57:56 +0200

    Added Checkout Item

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -2,10 +2,17 @@ import React from "react";
 
 import "./checkout-item.styles.scss";
 
-const CheckoutItem = ({img}) => <div className={'checkout-item'}>
+const CheckoutItem = ({ cartItem: { name, imageUrl, price, quantity } }) => (
+  <div className={"checkout-item"}>
     <div className="image-container">
-        <img src={img} alt="item"/>
+      <img src={imageUrl} alt="item" />
     </div>
-</div>;
+
+    <span className="name">{name}</span>
+    <span className="quantity">{quantity}</span>
+    <span className="price">{price}</span>
+    <span className="remove-button ">&#10005 </span>
+  </div>
+);
 
 export default CheckoutItem;
