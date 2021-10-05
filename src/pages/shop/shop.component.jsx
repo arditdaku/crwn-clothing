11ed7ba65c9a03f1243f90d6eb9b0330842e5c53
Ardit Daku <arditdaku1@gmail.com>
2021-10-05 09:37:14 +0200

    moved shop to state

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,29 +1,18 @@
-import React, {Component} from 'react';
-import SHOP_DATA from "./shop.data";
+import React, { Component } from "react";
 import CollectionPreview from "../../components/collection-preview/collection.preview.component";
-
-class ShopPage extends Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            collections: SHOP_DATA
-        }
-    }
-
-    render() {
-        const {collections} = this.state;
-        return (
-            <div className='shop-page'>
-                {
-                    collections.map(({id, ...otherCollectionProps}) => (
-                        <CollectionPreview key={id} {...otherCollectionProps} />
-                    ))
-                }
-            </div>
-        );
-    }
-}
-
-
-export default ShopPage;
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
+import { selectCollection } from "../../redux/shop/shop.selectors";
+const ShopPage = ({ collections }) => {
+  return (
+    <div className="shop-page">
+      {collections.map(({ id, ...otherCollectionProps }) => (
+        <CollectionPreview key={id} {...otherCollectionProps} />
+      ))}
+    </div>
+  );
+};
+const mapStateToProps = createStructuredSelector({
+  collections: selectCollection,
+});
+export default connect(mapStateToProps)(ShopPage);
