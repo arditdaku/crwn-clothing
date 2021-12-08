7a21a7342a5eae03f751997f5d545c864e5792cb
Ardit Daku <arditdaku1@gmail.com>
2021-12-08 13:32:07 +0100

    Added Redux saga & removed redux thunk

--- src/components/collections-overview/collections-overview.component.jsx
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -1,20 +1,22 @@
-import { connect } from 'react-redux'
-import { createStructuredSelector } from 'reselect'
-import { selectCollectionsForPreview } from '../../redux/shop/shop.selectors'
-import CollectionPreview from '../collection-preview/collection.preview.component'
-import './collections-overview.styles.scss'
+import React from 'react';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
 
-const CollectionsOverview = ({ collections }) => {
-    return (
-        <div className="collections-overview">
-            {collections.map(({ id, ...otherCollectionProps }) => (
-                <CollectionPreview key={id} {...otherCollectionProps} />
-            ))}
-        </div>
-    )
-}
+
+import { selectCollectionsForPreview } from '../../redux/shop/shop.selectors';
+import { CollectionsOverviewContainer } from './collections-overview.styles';
+import CollectionPreview from "../collection-preview/collection.preview.component";
+
+const CollectionsOverview = ({ collections }) => (
+    <CollectionsOverviewContainer>
+        {collections.map(({ id, ...otherCollectionProps }) => (
+            <CollectionPreview key={id} {...otherCollectionProps} />
+        ))}
+    </CollectionsOverviewContainer>
+);
 
 const mapStateToProps = createStructuredSelector({
-    collections: selectCollectionsForPreview,
-})
-export default connect(mapStateToProps)(CollectionsOverview)
+    collections: selectCollectionsForPreview
+});
+
+export default connect(mapStateToProps)(CollectionsOverview);
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,87 +1,40 @@
-import React, { Component } from 'react';
-
+import React from 'react';
 import { Route } from 'react-router-dom';
 import { connect } from 'react-redux';
 
-import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
-import CollectionPage from '../collection/collection.component';
+import { fetchCollectionsStart } from '../../redux/shop/shop.actions';
 
-import {
-    firestore,
-    convertCollectionsSnapshotToMap,
-} from '../../firebase/firebase.utils';
-import { updateCollections } from '../../redux/shop/shop.actions';
-
-import WithSpinner from '../../components/with-spinner/with-spinner.component';
-import CollectionsPage from '../collections/collections.componet';
-
-const CollectionsOverviewWithSpinner = WithSpinner(CollectionsOverview);
-const CollectionPageWithSpinner = WithSpinner(CollectionPage);
+import CollectionsOverviewContainer from '../../components/collections-overview/collections-overview.container';
+import CollectionPageContainer from '../collection/collection.container';
 
 class ShopPage extends React.Component {
-    state = {
-        loading: true,
-    };
-
-    unsubscribeFromSnapshot = null;
-
     componentDidMount() {
-        const { updateCollections } = this.props;
-        const collectionRef = firestore.collection('collections');
+        const { fetchCollectionsStart } = this.props;
 
-        collectionRef.get().then(snapshot => {
-            const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
-            updateCollections(collectionsMap);
-            this.setState({ loading: false });
-        });
-        // collectionsRef.get().then(async (snapshot) => {
-        //     const collectionsMap =
-        //         convertCollectionsSnapshotToMap(snapshot);
-        //     updateCollections(collectionsMap);
-        //     this.setState({ loading: false });
-        // }
-
-        // fetch(
-        //     ' https://firestore.googleapis.com/v1/projects/crw-db-11cd8/databases/(default)/documents/collections'
-        // )
-        //     .then((response) => response.json())
-        //     .then((collections) => console.log(collections));
+        fetchCollectionsStart();
     }
 
     render() {
         const { match } = this.props;
-        const { props } = this.state;
+
         return (
             <div className="shop-page">
                 <Route
                     exact
                     path={`${match.path}`}
-                    render={(props) => (
-                        <CollectionsOverviewWithSpinner
-                            isLoading={this.state.loading}
-                            {...props}
-                        />
-                    )}
+                    component={CollectionsOverviewContainer}
                 />
                 <Route
                     path={`${match.path}/:collectionId`}
-                    render={(props) => (
-                        <CollectionPageWithSpinner
-                            isLoading={this.state.loading}
-                            {...props}
-                        />
-                    )}
+                    component={CollectionPageContainer}
                 />
             </div>
         );
     }
 }
 
-const mapDispatchToProps = dispatch => ({
-    updateCollections: collectionsMap =>
-        dispatch(updateCollections(collectionsMap))
+const mapDispatchToProps = (dispatch) => ({
+    fetchCollectionsStart: () => dispatch(fetchCollectionsStart()),
 });
-export default connect(
-    null,
-    mapDispatchToProps
-)(ShopPage);
+
+export default connect(null, mapDispatchToProps)(ShopPage);
